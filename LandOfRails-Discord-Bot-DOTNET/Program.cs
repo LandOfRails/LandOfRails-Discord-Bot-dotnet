a4f7572f9fca886a92daafc0d3ace69af55f1c82
MarkenJaden <jjsch1410@gmail.com>
2021-05-08 00:50:42 +0200

    Projektdateien hinzufügen.

---
+++ LandOfRails-Discord-Bot-DOTNET/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace LandOfRails_Discord_Bot_DOTNET
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = await File.ReadAllTextAsync("Sensitive-data");
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
