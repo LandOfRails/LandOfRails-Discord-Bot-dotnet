58e7eb9810e1e651b677051e6a11e83732eb01d7
MarkenJaden <jjsch1410@gmail.com>
2021-07-20 15:00:35 +0200

    Added new commands

--- LandOfRails-Discord-Bot-DOTNET/Modules/ModpackCommands.cs
+++ LandOfRails-Discord-Bot-DOTNET/Modules/ModpackCommands.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Net;
@@ -11,7 +10,6 @@ using LandOfRails_Discord_Bot_DOTNET.Models;
 using LandOfRails_Discord_Bot_DOTNET.Utils;
 using Microsoft.EntityFrameworkCore.Internal;
 using Newtonsoft.Json;
-using Org.BouncyCastle.Crypto.Engines;
 
 namespace LandOfRails_Discord_Bot_DOTNET.Modules
 {
@@ -27,6 +25,8 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
 
             UpdateModpack();
             AddPermission();
+            RemovePermission();
+            UpdateImageLink();
 
             _discord.InteractionCreated += DiscordOnInteractionCreated;
         }
@@ -48,11 +48,12 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
                             var newVersion = new Version(command.Data.Options.ElementAt(2).Value.ToString()!);
                             if (modpackVersion < newVersion)
                             {
-
+                                //TODO
                             }
                             else
                             {
-                                await command.RespondAsync($"Please specify a higher version than {modpackVersion}. For example {modpackVersion.IncrementBuild()}");
+                                var build = modpackVersion.Build == 0 ? modpackVersion.Build : 1;
+                                await command.RespondAsync($"Please specify a higher version than {modpackVersion}. For example {modpackVersion.Major}.{modpackVersion.Minor}.{build}");
                             }
                         }
                         else await command.RespondAsync("You don't have permission to use this command for this modpack.");
@@ -77,6 +78,40 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
                             await context.SaveChangesAsync();
                             await command.RespondAsync($"{user} now has all permissions to the {command.Data.Options.ElementAt(0).Value} modpack.");
                         }
+                        else await command.RespondAsync("You don't have permission to use this command for this modpack. If you think this is wrong contact MarkenJaden.");
+
+                        break;
+                    case "remove-permission":
+                        if (command.User.Id == 222733101770604545 || context.Launchers.Any(x =>
+                            x.FkMemberId == (long)command.User.Id &&
+                            x.ModpackShortcut.Equals(command.Data.Options.ElementAt(0).Value)))
+                        {
+                            var user = command.Data.Options.ElementAt(1).Value as SocketUser;
+                            if (context.Launchers.Any(x => x.FkMemberId == (long)user.Id && x.ModpackShortcut.Equals(command.Data.Options.ElementAt(0).Value.ToString())))
+                            {
+                                context.Launchers.Remove(context.Launchers.First(x => x.FkMemberId == (long) user.Id));
+                                await context.SaveChangesAsync();
+                                await command.RespondAsync(
+                                    $"{user} was revoked the permissions to the {command.Data.Options.ElementAt(0).Value} modpack.");
+                                break;
+                            }
+                            await command.RespondAsync($"{user} has no permissions to the {command.Data.Options.ElementAt(0).Value} modpack.");
+                        }
+                        else await command.RespondAsync("You don't have permission to use this command for this modpack. If you think this is wrong contact MarkenJaden.");
+
+                        break;
+                    case "update-image-link":
+                        if (command.User.Id == 222733101770604545 || context.Launchers.Any(x =>
+                            x.FkMemberId == (long)command.User.Id &&
+                            x.ModpackShortcut.Equals(command.Data.Options.ElementAt(0).Value)))
+                        {
+                            if (Uri.TryCreate(command.Data.Options.ElementAt(0).Value.ToString(), UriKind.Absolute, out var uriResult) 
+                                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+                            {
+                                //TODO
+                            }
+                            else await command.RespondAsync("Invalid link! Please provide a valid link to the image.");
+                        }
                         else
                         {
                             await command.RespondAsync(
@@ -91,7 +126,7 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
 
         private void UpdateModpack()
         {
-            var updateModpackCommand = new SlashCommandBuilder()
+            var command = new SlashCommandBuilder()
                 .WithName("update-modpack")
                 .WithDescription("Updates a specific modpack to a new version in the LandOfRails Launcher.")
                 .AddOption(new SlashCommandOptionBuilder()
@@ -117,7 +152,7 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
 
             try
             {
-                _discord.Rest.CreateGlobalCommand(updateModpackCommand.Build());
+                _discord.Rest.CreateGlobalCommand(command.Build());
             }
             catch (ApplicationCommandException exception)
             {
@@ -131,7 +166,7 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
 
         private void AddPermission()
         {
-            var addPermissionCommand = new SlashCommandBuilder()
+            var command = new SlashCommandBuilder()
                 .WithName("add-permission")
                 .WithDescription("Add permission to edit modpack in the LandOfRails Launcher.")
                 .AddOption(new SlashCommandOptionBuilder()
@@ -151,7 +186,75 @@ namespace LandOfRails_Discord_Bot_DOTNET.Modules
                 );
             try
             {
-                _discord.Rest.CreateGlobalCommand(addPermissionCommand.Build());
+                _discord.Rest.CreateGlobalCommand(command.Build());
+            }
+            catch (ApplicationCommandException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Error, Formatting.Indented);
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(json);
+            }
+        }
+
+        private void RemovePermission()
+        {
+            var command = new SlashCommandBuilder()
+                .WithName("remove-permission")
+                .WithDescription("Remove permission to edit modpack in the LandOfRails Launcher.")
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("server")
+                    .WithDescription("The server which belongs to the modpack.")
+                    .WithRequired(true)
+                    .AddChoice("Traincraft", "tc")
+                    .AddChoice("Immersive Railroading", "ir")
+                    .AddChoice("Real Train Mod", "rtm")
+                    .AddChoice("Zora no Densha", "znd")
+                    .WithType(ApplicationCommandOptionType.String)
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("user")
+                    .WithDescription("User who should get permissions removed for the modpack.")
+                    .WithRequired(true)
+                    .WithType(ApplicationCommandOptionType.User)
+                );
+            try
+            {
+                _discord.Rest.CreateGlobalCommand(command.Build());
+            }
+            catch (ApplicationCommandException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Error, Formatting.Indented);
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(json);
+            }
+        }
+        
+        private void UpdateImageLink()
+        {
+            var command = new SlashCommandBuilder()
+                .WithName("update-image-link")
+                .WithDescription("Change the little modpack icon image.")
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("server")
+                    .WithDescription("The server which belongs to the modpack.")
+                    .WithRequired(true)
+                    .AddChoice("Traincraft", "tc")
+                    .AddChoice("Immersive Railroading", "ir")
+                    .AddChoice("Real Train Mod", "rtm")
+                    .AddChoice("Zora no Densha", "znd")
+                    .WithType(ApplicationCommandOptionType.String)
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("image-link")
+                    .WithDescription("Direct link to the image.")
+                    .WithRequired(true)
+                    .WithType(ApplicationCommandOptionType.String)
+                );
+            try
+            {
+                _discord.Rest.CreateGlobalCommand(command.Build());
             }
             catch (ApplicationCommandException exception)
             {
